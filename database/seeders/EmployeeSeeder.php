27ceb46909a29884079e9e5fde23521895396fe0
ItsMee04 <meemeeits@gmail.com>
2024-05-15 14:07:49 +0700

    First Commit

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Employee::create([
+            'name'          =>  'Indra Kusuma',
+            'address'       =>  'Purwokerto',
+            'phone'         =>  '081390469322',
+            'profession_id' =>  1,
+            'status'        => 1,
+            'created_at'     => now(),
+            'updated_at'    => now(),
+        ]);
+    }
+}
