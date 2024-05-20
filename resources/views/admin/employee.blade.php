7a365a07fe5f3e5b665e7516550afda479c6e820
ItsMee04 <meemeeits@gmail.com>
2024-05-20 07:35:58 +0700

    First Commit

--- resources/views/admin/employee.blade.php
+++ resources/views/admin/employee.blade.php
@@ -21,7 +21,8 @@
                     </li>
                 </ul>
                 <div class="page-btn">
-                    <a href="#" class="btn btn-added"><i data-feather="plus-circle" class="me-2"></i>Add
+                    <a href="#" class="btn btn-added" data-bs-toggle="modal" data-bs-target="#add-employee"><i
+                            data-feather="plus-circle" class="me-2"></i>Add
                         New Employee</a>
                 </div>
             </div>
@@ -58,12 +59,18 @@
                                         <td>
                                             <div class="userimgname">
                                                 <a href="javascript:void(0);" class="product-img">
-                                                    <img src="" alt="product">
+                                                    <img src="{{ asset('storage/avatar/' . $item->avatar) }}"
+                                                        alt="product">
                                                 </a>
-                                                <a href="javascript:void(0);">Arroon</a>
+                                                <a href="javascript:void(0);">{{ $item->name }}</a>
                                             </div>
                                         </td>
-                                        <td>Status</td>
+                                        <td>
+                                            @if ($item->status == 1)
+                                                <span class="badge bg-success">Active</span>
+                                            @else
+                                                <span class="badge bg-danger">Inactive</span>
+                                            @endif
                                         <td class="action-table-data">
                                             <div class="edit-delete-action">
                                                 <a class="me-2 edit-icon  p-2" href="product-details.html">
@@ -85,6 +92,61 @@
                 </div>
             </div>
 
+        </div>
+    </div>
+
+    <div class="modal fade" id="add-employee">
+        <div class="modal-dialog modal-dialog-centered custom-modal-two">
+            <div class="modal-content">
+                <div class="page-wrapper-new p-0">
+                    <div class="content">
+                        <div class="modal-header border-0 custom-modal-header">
+                            <div class="page-title">
+                                <h4>Create Sub Category</h4>
+                            </div>
+                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body custom-modal-body">
+                            <form action="sub-categories.html">
+                                <div class="mb-3">
+                                    <label class="form-label">Parent Category</label>
+                                    <select class="select">
+                                        <option>Choose Category</option>
+                                        <option>Category</option>
+                                    </select>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Category Name</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Category Code</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="mb-3 input-blocks">
+                                    <label class="form-label">Description</label>
+                                    <textarea class="form-control"></textarea>
+                                </div>
+                                <div class="mb-0">
+                                    <div
+                                        class="status-toggle modal-status d-flex justify-content-between align-items-center">
+                                        <span class="status-label">Status</span>
+                                        <input type="checkbox" id="user2" class="check" checked>
+                                        <label for="user2" class="checktoggle"></label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer-btn">
+                                    <button type="button" class="btn btn-cancel me-2"
+                                        data-bs-dismiss="modal">Cancel</button>
+                                    <button type="submit" class="btn btn-submit">Create Subcategory</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
